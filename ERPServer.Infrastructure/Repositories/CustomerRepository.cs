400a324329b706d880d7e88807d0bf42839d3500
Zehra Seren <fatmazehraseren@gmail.com>
2025-12-15 00:15:56 +0300

    feat(repository): implement Customer repository

---
+++ ERPServer.Infrastructure/Repositories/CustomerRepository.cs
@@ -0,0 +1,13 @@
+﻿using GenericRepository;
+using ERPServer.Domain.Entities;
+using ERPServer.Domain.Repositories;
+using ERPServer.Infrastructure.Context;
+
+namespace ERPServer.Infrastructure.Repositories;
+
+internal sealed class CustomerRepository : Repository<Customer, ApplicationDbContext>, ICustomerRepository
+{
+    public CustomerRepository(ApplicationDbContext context) : base(context)
+    {
+    }
+}
