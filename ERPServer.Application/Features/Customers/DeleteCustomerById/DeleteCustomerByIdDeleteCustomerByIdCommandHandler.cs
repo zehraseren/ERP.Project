5cd3a34f143e06709b19156332f122f7ff33adb4
Zehra Seren <fatmazehraseren@gmail.com>
2025-12-15 21:53:22 +0300

    feat(customer): implement delete customer use case

---
+++ ERPServer.Application/Features/Customers/DeleteCustomerById/DeleteCustomerByIdDeleteCustomerByIdCommandHandler.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using TS.Result;
+using GenericRepository;
+using ERPServer.Domain.Entities;
+using ERPServer.Domain.Repositories;
+
+namespace ERPServer.Application.Features.Customers.DeleteCustomerById;
+
+public sealed class DeleteCustomerByIdDeleteCustomerByIdCommandHandler(ICustomerRepository customerRepository, IUnitOfWork unitOfWork) : IRequestHandler<DeleteCustomerByIdCommand, Result<string>>
+{
+    public async Task<Result<string>> Handle(DeleteCustomerByIdCommand request, CancellationToken cancellationToken)
+    {
+        Customer customer = await customerRepository.GetByExpressionAsync(p => p.Id == request.Id, cancellationToken);
+
+        if (customer is null)
+        {
+            return Result<string>.Failure("Müşteri bulunamadı!");
+        }
+
+        customerRepository.Delete(customer);
+        await unitOfWork.SaveChangesAsync();
+
+        return "Müşteri başarıyla silindi.";
+    }
+}
