a158c1148e4169921e2d40f256ad50c76f6aed52
Zehra Seren <fatmazehraseren@gmail.com>
2025-12-16 16:27:47 +0300

    feat(depot): implement delete depot use case

---
+++ ERPServer.Application/Features/Depots/DeleteDepotById/DeleteDeportByIdCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using TS.Result;
+using GenericRepository;
+using ERPServer.Domain.Entities;
+using ERPServer.Domain.Repositories;
+
+namespace ERPServer.Application.Features.Depots.DeleteDepotById;
+
+internal sealed class DeleteDeportByIdCommandHandler(
+    IDepotRepository depotRepository,
+    IUnitOfWork unitOfWork) : IRequestHandler<DeleteDepotByIdCommand, Result<string>>
+{
+    public async Task<Result<string>> Handle(DeleteDepotByIdCommand request, CancellationToken cancellationToken)
+    {
+        Depot depot = await depotRepository.GetByExpressionAsync(p => p.Id == request.Id, cancellationToken);
+
+        if (depot is null)
+        {
+            return Result<string>.Failure("Depo bulunamadı!");
+        }
+
+        depotRepository.Delete(depot);
+        await unitOfWork.SaveChangesAsync(cancellationToken);
+
+        return "Depo başarıyla silindi.";
+    }
+}
--- ERPServer.WebAPI/Controllers/DepotsController.cs
+++ ERPServer.WebAPI/Controllers/DepotsController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using ERPServer.WebAPI.Abstractions;
 using ERPServer.Application.Features.Depots.GetAllDepot;
 using ERPServer.Application.Features.Depots.CreateDepot;
+using ERPServer.Application.Features.Depots.DeleteDepotById;
 
 namespace ERPServer.WebAPI.Controllers;
 
@@ -22,6 +23,14 @@ public sealed class DepotsController : ApiController
 
     [HttpPost]
     public async Task<IActionResult> Create(CreateDepotCommand request, CancellationToken cancellationToken)
+    {
+        var response = await _mediator.Send(request, cancellationToken);
+
+        return StatusCode(response.StatusCode, response);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> DeleteById(DeleteDepotByIdCommand request, CancellationToken cancellationToken)
     {
         var response = await _mediator.Send(request, cancellationToken);
 
