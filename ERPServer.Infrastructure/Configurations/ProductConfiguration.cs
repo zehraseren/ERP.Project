921bc9dbdde86466b6fd19ae72f27aca47741ba8
Zehra Seren <fatmazehraseren@gmail.com>
2025-12-18 16:32:17 +0300

    feat(config): implement Product entity configuration

---
+++ ERPServer.Infrastructure/Configurations/ProductConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using ERPServer.Domain.Enums;
+using ERPServer.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ERPServer.Infrastructure.Configurations;
+
+internal sealed class ProductConfiguration : IEntityTypeConfiguration<Product>
+{
+    public void Configure(EntityTypeBuilder<Product> builder)
+    {
+        builder.Property(p => p.Type)
+            .HasConversion(type => type.Value, value => ProductTypeEnum.FromValue(value));
+    }
+}
