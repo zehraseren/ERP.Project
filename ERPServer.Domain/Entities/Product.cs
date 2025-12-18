73af21de7ddf401a22b518402640430fbd0f97a2
Zehra Seren <fatmazehraseren@gmail.com>
2025-12-18 16:02:04 +0300

    feat(entity): add Product entity

---
+++ ERPServer.Domain/Entities/Product.cs
@@ -0,0 +1,10 @@
+﻿using ERPServer.Domain.Enums;
+using ERPServer.Domain.Abstractions;
+
+namespace ERPServer.Domain.Entities;
+
+public sealed class Product : Entity
+{
+    public string Name { get; set; } = string.Empty;
+    public ProductTypeEnum Type { get; set; } = ProductTypeEnum.Product;
+}
