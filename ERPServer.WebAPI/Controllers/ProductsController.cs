df07d5471edc7e89bdcff0dff944ada143b828b3
Zehra Seren <fatmazehraseren@gmail.com>
2025-12-18 17:35:49 +0300

    feat(product): implement delete product use case

--- ERPServer.WebAPI/Controllers/ProductsController.cs
+++ ERPServer.WebAPI/Controllers/ProductsController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using ERPServer.WebAPI.Abstractions;
 using ERPServer.Application.Features.Products.GetAllProduct;
 using ERPServer.Application.Features.Products.CreateProduct;
+using ERPServer.Application.Features.Products.DeleteProductById;
 
 namespace ERPServer.WebAPI.Controllers;
 
@@ -22,6 +23,14 @@ public class ProductsController : ApiController
 
     [HttpPost]
     public async Task<IActionResult> Create(CreateProductCommand request, CancellationToken cancellationToken)
+    {
+        var response = await _mediator.Send(request, cancellationToken);
+
+        return StatusCode(response.StatusCode, response);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> DeleteById(DeleteProductByIdCommand request, CancellationToken cancellationToken)
     {
         var response = await _mediator.Send(request, cancellationToken);
 
