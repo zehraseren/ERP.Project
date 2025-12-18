277ffa6e493c4234bbf7b8c8feb2eb850feed449
Zehra Seren <fatmazehraseren@gmail.com>
2025-12-18 16:44:44 +0300

    feat(repository): implement Product repository

---
+++ ERPServer.Infrastructure/Repositories/ProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using GenericRepository;
+using ERPServer.Domain.Entities;
+using ERPServer.Domain.Repositories;
+using ERPServer.Infrastructure.Context;
+
+namespace ERPServer.Infrastructure.Repositories;
+
+internal sealed class ProductRepository : Repository<Product, ApplicationDbContext>, IProductRepository
+{
+    public ProductRepository(ApplicationDbContext context) : base(context)
+    {
+    }
+}
