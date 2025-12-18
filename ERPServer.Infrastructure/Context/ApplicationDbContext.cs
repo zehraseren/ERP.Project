65b1f62d41d25128ad0dd9557cc66b7265255106
Zehra Seren <fatmazehraseren@gmail.com>
2025-12-18 16:32:54 +0300

    feat(dbcontext): register new entity in DbContext

--- ERPServer.Infrastructure/Context/ApplicationDbContext.cs
+++ ERPServer.Infrastructure/Context/ApplicationDbContext.cs
@@ -14,6 +14,7 @@ namespace ERPServer.Infrastructure.Context
 
         public DbSet<Customer> Customers { get; set; }
         public DbSet<Depot> Depots { get; set; }
+        public DbSet<Product> Products { get; set; }
         protected override void OnModelCreating(ModelBuilder builder)
         {
             builder.ApplyConfigurationsFromAssembly(typeof(DependencyInjection).Assembly);
