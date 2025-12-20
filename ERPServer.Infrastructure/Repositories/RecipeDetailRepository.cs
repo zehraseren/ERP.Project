1c37ffe6a96fe0df105896064eaa4167f1df8327
Zehra Seren <fatmazehraseren@gmail.com>
2025-12-20 17:02:13 +0300

    feat(repository): implement Recipe and RecipeDetail repositories

---
+++ ERPServer.Infrastructure/Repositories/RecipeDetailRepository.cs
@@ -0,0 +1,13 @@
+﻿using GenericRepository;
+using ERPServer.Domain.Entities;
+using ERPServer.Domain.Repositories;
+using ERPServer.Infrastructure.Context;
+
+namespace ERPServer.Infrastructure.Repositories;
+
+internal sealed class RecipeDetailRepository : Repository<RecipeDetail, ApplicationDbContext>, IRecipeDetailRepository
+{
+    public RecipeDetailRepository(ApplicationDbContext context) : base(context)
+    {
+    }
+}
---
+++ ERPServer.Infrastructure/Repositories/RecipeRepository.cs
@@ -0,0 +1,13 @@
+﻿using GenericRepository;
+using ERPServer.Domain.Entities;
+using ERPServer.Domain.Repositories;
+using ERPServer.Infrastructure.Context;
+
+namespace ERPServer.Infrastructure.Repositories;
+
+internal sealed class RecipeRepository : Repository<Recipe, ApplicationDbContext>, IRecipeRepository
+{
+    public RecipeRepository(ApplicationDbContext context) : base(context)
+    {
+    }
+}
