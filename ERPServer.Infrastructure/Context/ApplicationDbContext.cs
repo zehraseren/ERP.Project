414560a5c874419be8be8fd2dad39e91c6d14d3c
Zehra Seren <fatmazehraseren@gmail.com>
2025-12-28 14:10:43 +0300

    feat(dbcontext): register Order and OrderDetail entities in DbContext

--- ERPServer.Infrastructure/Context/ApplicationDbContext.cs
+++ ERPServer.Infrastructure/Context/ApplicationDbContext.cs
@@ -17,6 +17,8 @@ namespace ERPServer.Infrastructure.Context
         public DbSet<Product> Products { get; set; }
         public DbSet<Recipe> Recipes { get; set; }
         public DbSet<RecipeDetail> RecipeDetails { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
