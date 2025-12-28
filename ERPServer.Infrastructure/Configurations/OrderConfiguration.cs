1691206cebb7c11dcd51ed2cdabb9486a38b8e8d
Zehra Seren <fatmazehraseren@gmail.com>
2025-12-28 14:08:53 +0300

    feat(config): implement Order and OrderDetail entity configurations

---
+++ ERPServer.Infrastructure/Configurations/OrderConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using ERPServer.Domain.Enums;
+using ERPServer.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ERPServer.Infrastructure.Configurations;
+
+internal sealed class OrderConfiguration : IEntityTypeConfiguration<Order>
+{
+    public void Configure(EntityTypeBuilder<Order> builder)
+    {
+        builder.Property(p => p.Status)
+            .HasConversion(status => status.Value, value => OrderStatusEnum.FromValue(value));
+    }
+}
