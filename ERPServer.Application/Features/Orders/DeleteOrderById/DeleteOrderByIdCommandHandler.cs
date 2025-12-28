1776d2c3d73cadddccc4b751b4699e9ceb81c006
Zehra Seren <fatmazehraseren@gmail.com>
2025-12-28 14:14:00 +0300

    feat(order): implement delete order use case

---
+++ ERPServer.Application/Features/Orders/DeleteOrderById/DeleteOrderByIdCommandHandler.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using TS.Result;
+using GenericRepository;
+using ERPServer.Domain.Entities;
+using ERPServer.Domain.Repositories;
+
+namespace ERPServer.Application.Features.Orders.DeleteOrderById;
+
+internal sealed class DeleteOrderByIdCommandHandler(
+    IOrderRepository orderRepository,
+    IUnitOfWork unitOfWork) : IRequestHandler<DeleteOrderByIdCommand, Result<string>>
+{
+    public async Task<Result<string>> Handle(DeleteOrderByIdCommand request, CancellationToken cancellationToken)
+    {
+        Order order = await orderRepository.GetByExpressionAsync(p => p.Id == request.Id, cancellationToken);
+
+        if (order is null) return Result<string>.Failure("Sipariş bulunamadı!");
+
+        orderRepository.Delete(order);
+        await unitOfWork.SaveChangesAsync(cancellationToken);
+
+        return "Sipariş başarıyla silindi.";
+    }
+}
--- ERPServer.WebAPI/Controllers/OrdersController.cs
+++ ERPServer.WebAPI/Controllers/OrdersController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using ERPServer.WebAPI.Abstractions;
 using ERPServer.Application.Features.Orders.GetAllOrder;
 using ERPServer.Application.Features.Orders.CreateOrder;
+using ERPServer.Application.Features.Orders.DeleteOrderById;
 
 namespace ERPServer.WebAPI.Controllers;
 
@@ -22,6 +23,14 @@ public class OrdersController : ApiController
 
     [HttpPost]
     public async Task<IActionResult> Create(CreateOrderCommand request, CancellationToken cancellationToken)
+    {
+        var response = await _mediator.Send(request, cancellationToken);
+
+        return StatusCode(response.StatusCode, response);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> DeleteById(DeleteOrderByIdCommand request, CancellationToken cancellationToken)
     {
         var response = await _mediator.Send(request, cancellationToken);
 
