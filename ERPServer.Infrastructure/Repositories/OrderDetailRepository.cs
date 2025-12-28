d04d631dc1d91fc16e024dbedad33348b80964f1
Zehra Seren <fatmazehraseren@gmail.com>
2025-12-28 14:11:29 +0300

    feat(repository): implement Order and OrderDetail repositories

---
+++ ERPServer.Infrastructure/Repositories/OrderDetailRepository.cs
@@ -0,0 +1,13 @@
+﻿using GenericRepository;
+using ERPServer.Domain.Entities;
+using ERPServer.Domain.Repositories;
+using ERPServer.Infrastructure.Context;
+
+namespace ERPServer.Infrastructure.Repositories;
+
+internal class OrderDetailRepository : Repository<OrderDetail, ApplicationDbContext>, IOrderDetailRepository
+{
+    public OrderDetailRepository(ApplicationDbContext context) : base(context)
+    {
+    }
+}
