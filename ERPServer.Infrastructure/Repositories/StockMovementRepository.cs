3708ffb1ce65d33d213bc245e369c09586902310
Zehra Seren <fatmazehraseren@gmail.com>
2025-12-28 17:13:40 +0300

    feat(repository): implement StockMovement repository

---
+++ ERPServer.Infrastructure/Repositories/StockMovementRepository.cs
@@ -0,0 +1,13 @@
+﻿using GenericRepository;
+using ERPServer.Domain.Entities;
+using ERPServer.Domain.Repositories;
+using ERPServer.Infrastructure.Context;
+
+namespace ERPServer.Infrastructure.Repositories;
+
+internal class StockMovementRepository : Repository<StockMovement, ApplicationDbContext>, IStockMovementRepository
+{
+    public StockMovementRepository(ApplicationDbContext context) : base(context)
+    {
+    }
+}
