a46d406b4dc29b4bdbc6512925a5da0e8f9990be
Zehra Seren <fatmazehraseren@gmail.com>
2025-12-29 21:56:00 +0300

    feat(config): implement Invoice and InvoiceDetail entities configurations

---
+++ ERPServer.Infrastructure/Configurations/InvoiceConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using ERPServer.Domain.Enums;
+using ERPServer.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ERPServer.Infrastructure.Configurations;
+
+internal sealed class InvoiceConfiguration : IEntityTypeConfiguration<Invoice>
+{
+    public void Configure(EntityTypeBuilder<Invoice> builder)
+    {
+        builder.Property(p => p.Type)
+            .HasConversion(type => type.Value, value => InvoiceTypeEnum.FromValue(value));
+    }
+}
