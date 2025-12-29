2b6b38aeec0e7b3b07e72facdbc5799637c6396c
Zehra Seren <fatmazehraseren@gmail.com>
2025-12-29 22:04:55 +0300

    feat(repository): implement Invoice and InvoiceDetail repositories

---
+++ ERPServer.Infrastructure/Repositories/InvoiceDetailRepository.cs
@@ -0,0 +1,13 @@
+﻿using GenericRepository;
+using ERPServer.Domain.Entities;
+using ERPServer.Domain.Repositories;
+using ERPServer.Infrastructure.Context;
+
+namespace ERPServer.Infrastructure.Repositories;
+
+internal sealed class InvoiceDetailRepository : Repository<InvoiceDetail, ApplicationDbContext>, IInvoiceDetailRepository
+{
+    public InvoiceDetailRepository(ApplicationDbContext context) : base(context)
+    {
+    }
+}
---
+++ ERPServer.Infrastructure/Repositories/InvoiceRepository.cs
@@ -0,0 +1,13 @@
+﻿using GenericRepository;
+using ERPServer.Domain.Entities;
+using ERPServer.Domain.Repositories;
+using ERPServer.Infrastructure.Context;
+
+namespace ERPServer.Infrastructure.Repositories;
+
+internal sealed class InvoiceRepository : Repository<Invoice, ApplicationDbContext>, IInvoiceRepository
+{
+    public InvoiceRepository(ApplicationDbContext context) : base(context)
+    {
+    }
+}
