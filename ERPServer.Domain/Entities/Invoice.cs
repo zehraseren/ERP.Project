9d59503069cee2a42703eecb47668841e6fd3938
Zehra Seren <fatmazehraseren@gmail.com>
2025-12-29 21:55:43 +0300

    feat(entity): add Invoice and InvoiceDetail entities

---
+++ ERPServer.Domain/Entities/Invoice.cs
@@ -0,0 +1,15 @@
+﻿using ERPServer.Domain.Enums;
+using ERPServer.Domain.Abstractions;
+
+namespace ERPServer.Domain.Entities;
+
+public sealed class Invoice : Entity
+{
+    public Guid CustomerId { get; set; }
+    public Customer? Customer { get; set; }
+    public string InvoiceNumber { get; set; } = string.Empty;
+    public DateOnly Date { get; set; }
+    public InvoiceTypeEnum Type { get; set; } = InvoiceTypeEnum.Purchase;
+    public List<InvoiceDetail>? InvoiceDetails { get; set; }
+}
+
