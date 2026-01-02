3bd0df4e5b60ef120400abaa17f71c926f745bf4
Zehra Seren <fatmazehraseren@gmail.com>
2026-01-02 22:19:56 +0300

    refactor(entity): add new property to StockMovement and update migration

---
+++ ERPServer.Infrastructure/Migrations/20260102174904_AddProductionIdToStockMovement.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ERPServer.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProductionIdToStockMovement : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "ProductionId",
+                table: "StockMovements",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StockMovements_ProductionId",
+                table: "StockMovements",
+                column: "ProductionId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_StockMovements_Productions_ProductionId",
+                table: "StockMovements",
+                column: "ProductionId",
+                principalTable: "Productions",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_StockMovements_Productions_ProductionId",
+                table: "StockMovements");
+
+            migrationBuilder.DropIndex(
+                name: "IX_StockMovements_ProductionId",
+                table: "StockMovements");
+
+            migrationBuilder.DropColumn(
+                name: "ProductionId",
+                table: "StockMovements");
+        }
+    }
+}
