49b3daebe8682697dec488228282341f1074962f
Zehra Seren <fatmazehraseren@gmail.com>
2026-01-02 22:20:06 +0300

    feat(repository): implement Production repository

---
+++ ERPServer.Infrastructure/Repositories/ProductionRepository.cs
@@ -0,0 +1,13 @@
+﻿using GenericRepository;
+using ERPServer.Domain.Entities;
+using ERPServer.Domain.Repositories;
+using ERPServer.Infrastructure.Context;
+
+namespace ERPServer.Infrastructure.Repositories;
+
+internal sealed class ProductionRepository : Repository<Production, ApplicationDbContext>, IProductionRepository
+{
+    public ProductionRepository(ApplicationDbContext context) : base(context)
+    {
+    }
+}
