2992bae0aa50132aba07e26809a11d19cfcab653
Zehra Seren <fatmazehraseren@gmail.com>
2026-01-07 17:19:52 +0300

    feat(stock): implement stock quantity tracking

--- ERPServer.Application/Features/Products/GetAllProduct/GetAllProductQuery.cs
+++ ERPServer.Application/Features/Products/GetAllProduct/GetAllProductQuery.cs
@@ -1,7 +1,6 @@
 ﻿using MediatR;
 using TS.Result;
-using ERPServer.Domain.Entities;
 
 namespace ERPServer.Application.Features.Products.GetAllProduct;
 
-public sealed record GetAllProductQuery : IRequest<Result<List<Product>>>;
+public sealed record GetAllProductQuery : IRequest<Result<List<GetAllProductQueryResponse>>>;
